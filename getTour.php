0f82fdf03ee85dff11fb9bf3d2da3a302147c60d
Mariana Marunchak <mariana.mrnch@gmail.com>
2024-12-14 23:33:16 +0200

    API to tours

--- getTour.php
+++ getTour.php
@@ -1,5 +1,5 @@
 <?php
-header('Content-Type: application/json; charset=utf-8');
+header('Content-Type: application/json; charset=utf-8'); // вказує браузеру, що відповідь від сервера буде у форматі JSON
 header('Access-Control-Allow-Origin: *'); // Дозволяє доступ з будь-якого джерела
 header('Access-Control-Allow-Methods: GET, POST, OPTIONS'); // Дозволяє методи GET, POST, OPTIONS
 header('Access-Control-Allow-Headers: Content-Type, Authorization'); // Дозволяє зазначені заголовки
@@ -13,10 +13,23 @@ try {
     $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    $stmt = $conn->query("SELECT title FROM tours LIMIT 1");
-    $tour = $stmt->fetch(PDO::FETCH_ASSOC);
+    $stmt = $conn->query("SELECT 
+        t.id, t.title, t.destination_location, t.start_date, t.people_limit, i.image_path
+    FROM tours t
+    LEFT JOIN images i ON t.id = i.tour_id 
+    WHERE i.object_type = 'tour' AND i.category = 'banner'");
 
-    echo json_encode($tour);
+    $tours = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    foreach ($tours as &$tour) {
+        if (isset($tour['image_path'])) {
+            // Формуємо абсолютний шлях до зображення
+            $tour['image_path'] = 'http://' . $_SERVER['SERVER_NAME'] . '/exploreia-backend' . $tour['image_path'];
+        }
+    }
+    unset($tour);
+
+    echo json_encode($tours);
 } catch (PDOException $e) {
     echo json_encode(['error' => $e->getMessage()]);
 }
